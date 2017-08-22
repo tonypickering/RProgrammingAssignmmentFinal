a58bc4d2cf4b5b7eb72cdd54322a60b7fce3f057
Tony <30377945+tonypickering@users.noreply.github.com>
2017-08-22 16:01:58 -0700

    attemting commit by dragging R template

---
+++ codeforfinal.R
@@ -0,0 +1,61 @@
+
+best <- function(state, outcome) { 
+  
+  ## Read outcome data
+  
+  
+  
+  
+  ## Check that state and outcome are valid
+  
+  
+  
+  
+  ## Return hospital name in that state with lowest 30-day death rate
+  
+  
+  
+  
+}
+
+
+rankhospital <- function(state, outcome, num = "best") {
+  
+  ## Read outcome data
+  
+  
+  
+  
+  ## Check that state and outcome are valid
+  
+  
+  
+  
+  ## Return hospital name in that state with the given rank ## 30-day death rate
+  
+  
+  
+  
+}
+
+
+
+rankall <- function(outcome, num = "best") {
+  
+  ## Read outcome data
+  
+  
+  
+  
+  ## Check that state and outcome are valid
+  
+  
+  
+  
+  ## For each state, find the hospital of the given rank
+  ## Return a data frame with the hospital names and the ## (abbreviated) state name
+  
+  
+  
+  
+}
